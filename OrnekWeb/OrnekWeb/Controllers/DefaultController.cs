f7e7db8870321540633e7b0dea2f5945f999a613
MuratYucedag <MurattYucedag@users.noreply.github.com>
2021-05-05 11:39:49 +0300

    Proje dosyaları yüklendi

---
+++ OrnekWeb/OrnekWeb/Controllers/DefaultController.cs
@@ -0,0 +1,48 @@
+﻿using OrnekWeb.Models.Sinif;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OrnekWeb.Models;
+
+namespace OrnekWeb.Controllers
+{
+    public class DefaultController : Controller
+    {
+        dataprojeEntities c = new dataprojeEntities();
+
+        public ActionResult Dinamik()
+        {
+            Class1 cs = new Class1();
+            cs.deger1 = c.Faturalars.ToList();
+            cs.deger2 = c.FaturaKalems.ToList();
+            return View(cs);
+        }
+        public ActionResult FaturaKaydet(string FaturaSeriNo, string FaturaSıraNo, DateTime Tarih, string VergiDairesi, string Saat, string TeslimEden, string TeslimAlan, string Toplam, FaturaKalems[] kalemler)
+        {
+            Faturalars f = new Faturalars();
+            f.FaturaSeriNo = FaturaSeriNo;
+            f.FaturaSıraNo = FaturaSıraNo;
+            f.Tarih = Tarih;
+            f.VergiDairesi = VergiDairesi;
+            f.Saat = Saat;
+            f.TeslimEden = TeslimEden;
+            f.TeslimAlan = TeslimAlan;
+            f.Toplam = decimal.Parse(Toplam);
+            c.Faturalars.Add(f);
+            foreach (var x in kalemler)
+            {
+                FaturaKalems fk = new FaturaKalems();
+                fk.Aciklama = x.Aciklama;
+                fk.BirimFiyat = x.BirimFiyat;
+                fk.Faturaid = x.FaturaKalemid;
+                fk.Miktar = x.Miktar;
+                fk.Tutar = x.Tutar;
+                c.FaturaKalems.Add(fk);
+            }
+            c.SaveChanges();
+            return Json("İşlem Başarılı", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ OrnekWeb/OrnekWeb/Models/Sinif/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrnekWeb.Models.Sinif
+{
+    public class Class1
+    {
+        public IEnumerable<Faturalars> deger1 { get; set; }
+        public IEnumerable<FaturaKalems> deger2 { get; set; }
+    }
+}
